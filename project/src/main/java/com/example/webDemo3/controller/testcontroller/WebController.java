dcc1fe604987db1509bb93eed9f83abf923c7a4e
yen <vubaoyen24101998@gmail.com>
2020-06-22 20:51:33 +0700

    xoa code thua

--- project/src/main/java/com/example/webDemo3/controller/testcontroller/WebController.java
+++ project/src/main/java/com/example/webDemo3/controller/testcontroller/WebController.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-//    @Autowired
-//    private UserRepository u;
-//
-//    @Autowired
-//    private TodoRepository todoRepository;
-    // Đón nhận request GET
+
     @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
     public String index(Model model) {
-//        List<Users> k = u.findAll() ;
-//        model.addAttribute("userList", k) ;
         return "index"; // Trả về file index.html
     }
 
