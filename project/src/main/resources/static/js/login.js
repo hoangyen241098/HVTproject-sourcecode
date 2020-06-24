95b8103d12db9870a840fe9d7af6ae2bb9734d8f
Jung Ki Ha Ji <haji98.hjjk@gmail.com>
2020-06-24 15:53:04 +0700

    Update Template Login

--- project/src/main/resources/static/js/login.js
+++ project/src/main/resources/static/js/login.js
@@ -6,16 +6,13 @@ $(document).ready(function () {
         var password = $('#password').val();
 
         if (username.trim() == "" && password.trim() == "") {
-            $('#error-username').text("Hãy điền tên đăng nhập.");
-            $('#error-password').text("Hãy điền mật khẩu.");
+            $('.errorTxt').text("Hãy điền tên đăng nhập và mật khẩu.");
             return false;
         } else if (username.trim() == "") {
-            $('#error-username').text("Hãy điền tên đăng nhập.");
-            $('#error-password').text("");
+            $('.errorTxt').text("Hãy điền tên đăng nhập.");
             return false;
         } else if (password.trim() == "") {
-            $('#error-username').text("");
-            $('#error-password').text("Hãy điền mật khẩu.");
+            $('.errorTxt').text("Hãy điền mật khẩu.");
             return false;
         } else {
             $('#error-username').text("");
@@ -37,11 +34,7 @@ $(document).ready(function () {
                     if (messageCode == 0) {
                         $('#loginSuccess').css('display', 'block');
                     } else {
-                        if (message.includes("Tên đăng nhập")) {
-                            $('#error-username').text(message);
-                        } else {
-                            $('#error-password').text(message);
-                        }
+                        $('.errorTxt').text(message);
                     }
                 },
                 failure: function (errMsg) {
@@ -53,85 +46,6 @@ $(document).ready(function () {
         }
     })
 });
-//
-// rules: {
-//     username: "required",
-//     password: "required"
-// },
-// messages: {
-//     username: "Hãy điền tên đăng nhập.",
-//     password: "Hãy điền mật khẩu."
-// },
-//
-// errorPlacement: function (error, element) {
-//     if (element.attr("name") == "username") {
-//         element.parent('div').parent('div').find("label.error-text").addClass("error").text($(error).text());
-//     }
-//     if (element.attr("name") == "password") {
-//         element.parent('div').parent('div').find("label.error-text").addClass("error").text($(error).text());
-//     } else {
-//         return true;
-//     }
-// },
-// submitHandler: function (form) {
-//         var user = {
-//             username: $('#username').val(),
-//             password: $('#password').val(),
-//         };
-//
-//         e.preventDefault();
-//         $.ajax({
-//             type: 'POST',
-//             url: "http://localhost:8080/api/user/login",
-//             data: JSON.stringify(user),
-//             success: function (data) {
-//                 var messageCode = data.message.messageCode;
-//                 var message = data.message.message;
-//                 if (messageCode == 0) {
-//                     $('#loginSuccess').css('display', 'block');
-//                 } else {
-//                     $('.errorTxt').text(message);
-//                 }
-//             },
-//             failure: function (errMsg) {
-//                 $('.errorTxt').text(errMsg);
-//             },
-//             dataType: "json",
-//             contentType: "application/json"
-//         });
-//     }
-// });
-// $("#signin").click(function (e) {
-//     var user = {
-//         username: $('#username').val(),
-//         password: $('#password').val(),
-//     };
-//
-//     e.preventDefault();
-//     $.ajax({
-//         type: 'POST',
-//         url: "http://localhost:8080/api/user/login",
-//         data: JSON.stringify(user),
-//         success: function (data) {
-//             var messageCode = data.message.messageCode;
-//             var message = data.message.message;
-//             if (messageCode == 0) {
-//                 $('#loginSuccess').css('display', 'block');
-//             } else {
-//                 $('.errorTxt').text(message);
-//             }
-//         },
-//         failure: function (errMsg) {
-//             $('.errorTxt').text(errMsg);
-//         },
-//         dataType: "json",
-//         contentType: "application/json"
-//     });
-//
-// });
-// })
-// ;
-
 
 // <!--Get data from JSON-->
 // (function () {
