6a75df346d77540f8c7940f1ada831df4805a6c4
yen <vubaoyen24101998@gmail.com>
2020-06-24 10:29:59 +0700

    add constant

---
+++ project/src/main/java/com/example/webDemo3/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.example.webDemo3.constant;
+
+import com.example.webDemo3.dto.MessageDTO;
+
+public class Constant {
+    public static final MessageDTO USER_NOT_EXIT =
+            new MessageDTO(0,"Tên đăng nhập không tồn tại");
+}
