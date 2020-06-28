0d16060fe57f454c6f43f5e3cd811d01760fac30
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2020-06-28 17:55:04 +0700

    kimpt142 commit fix sort class list + search user list 28/6

--- project/src/main/java/com/example/webDemo3/constant/Constant.java
+++ project/src/main/java/com/example/webDemo3/constant/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     /**
      * Constant value
      */
-    public static final Integer PAGE_SIZE = 5;
+    public static final Integer PAGE_SIZE = 10;
     /**
      * Fail message
      */
