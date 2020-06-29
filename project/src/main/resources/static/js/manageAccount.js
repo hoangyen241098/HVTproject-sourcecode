c7c383e94c45466d30a9153e7edefa2bc57b6f2b
Jung Ki Ha Ji <haji98.hjjk@gmail.com>
2020-06-29 16:49:41 +0700

    1. Check select all manageAccount page 2. Create function search in manageAccount page 3. Create resetPassword page 4. Create function delete account in manageAccount page

--- project/src/main/resources/static/js/manageAccount.js
+++ project/src/main/resources/static/js/manageAccount.js
@@ -1,11 +1,10 @@
+/*Load role list*/
 $.ajax({
     url: '/api/admin/rolelist',
     type: 'POST',
     success: function (data) {
-        $.each(data, function (i, item) {
-            $.each(item, function (i, list) {
-                $('#search-role').append(`<li value="` + list.roleId + `">` + list.roleName + `</li>`);
-            });
+        $.each(data.listRole, function (i, item) {
+            $('#search-role').append(`<li value="` + item.roleId + `">` + item.roleName + `</li>`);
         });
     },
     failure: function (errMsg) {
@@ -21,33 +20,78 @@ var inforSearch = {
     orderBy: "0",
     pageNumber: "0"
 }
-$.ajax({
-    url: '/api/admin/userlist',
-    type: 'POST',
-    data: JSON.stringify(inforSearch),
-    success: function (data) {
-        $.each(data.userList.content, function (i, item) {
-            var mappingName, phone, email;
-            if (item.classSchool == null) {
-                mappingName = "";
-            } else {
-                mappingName = item.classSchool.mappingName;
+var listUser = [];
+/*Search button*/
+$("#search").click(function () {
+    var userName, roleId, sortBy, orderBy, pageNumber;
+    userName = $('#searchByUsername input').val();
+    pageNumber = 0;
+    if ($('#role-name input').attr("value") == null) {
+        roleId = 0;
+    } else {
+        roleId = $('#role-name input').attr("value");
+    }
+    if ($('#sortBy input').attr("value") == null) {
+        sortBy = 0;
+    } else {
+        sortBy = $('#sortBy input').attr("value");
+    }
+    if ($('#orderBy input').attr("value") == null) {
+        orderBy = 0;
+    } else {
+        orderBy = $('#orderBy input').attr("value");
+    }
+    inforSearch = {
+        userName: userName,
+        roleId: roleId,
+        sortBy: sortBy,
+        orderBy: orderBy,
+        pageNumber: pageNumber
+    }
+    console.log(JSON.stringify(inforSearch));
+    $('tbody').html("");
+    search();
+});
+search();
+
+/*Load user list*/
+function search() {
+    $.ajax({
+        url: '/api/admin/userlist',
+        type: 'POST',
+        data: JSON.stringify(inforSearch),
+        success: function (data) {
+            if (data.userList.content.length == 0) {
+                $('tbody').append(
+                    `<tr>
+                        <td colspan="7" class="userlist-result">
+                            Không có kết quả trả về!
+                        </td>
+                    </tr>`
+                )
             }
-            if (item.phone == null) {
-                phone = "";
-            } else {
-                phone = item.phone;
-            }
-            if (item.email == null) {
-                email = "";
-            } else {
-                email = item.email;
-            }
-            $('tbody').append(
-                `<tr>
+            $.each(data.userList.content, function (i, item) {
+                var mappingName, phone, email;
+                if (item.classSchool == null) {
+                    mappingName = "";
+                } else {
+                    mappingName = item.classSchool.mappingName;
+                }
+                if (item.phone == null) {
+                    phone = "";
+                } else {
+                    phone = item.phone;
+                }
+                if (item.email == null) {
+                    email = "";
+                } else {
+                    email = item.email;
+                }
+                $('tbody').append(
+                    `<tr>
                 <td>
                     <span class="custom-checkbox">
-                        <input id="` + item.username + `"type="checkbox" name="options[]" value="1">
+                        <input id="` + item.username + `"type="checkbox" name="options" value="` + item.username + `">
                         <label for="` + item.username + `"></label>
                     </span>
                 </td>
@@ -57,24 +101,136 @@ $.ajax({
                 <td><span id="className">` + mappingName + `</span></td>
                 <td><span id="phone">` + phone + `</span></td>
                 <td><span id="email">` + email + `</span></td>
-                <td>
-                    <a th:href="@{editAccount/id}" class="edit" data-toggle="modal">
-                        <i class="material-icons" data-toggle="tooltip" title="Sửa">&#xE254;</i>
-                    </a>
-                </td>
             </tr>`);
-        });
-    },
-    failure: function (errMsg) {
-        console.log(errMsg);
-    },
-    dataType: "json",
-    contentType: "application/json"
+            });
+            selectCheckbox();
+        },
+        failure: function (errMsg) {
+            console.log(errMsg);
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
+}
+
+/*Delete account*/
+$("#deleteAccount").click(function (e) {
+    listUser = {
+        listUser: listUser,
+    }
+    e.preventDefault();
+    console.log(JSON.stringify(listUser));
+    $.ajax({
+        url: '/api/admin/deleteaccount',
+        type: 'DELETE',
+        data: JSON.stringify(listUser),
+        success: function (data) {
+            var messageCode = data.messageCode;
+            var message = data.message;
+            if (messageCode == 0) {
+                $('.img-success').removeClass('hide');
+                $('.img-fail').addClass('hide');
+                $('#message-delete').text(message);
+            } else {
+                $('.img-success').addClass('hide');
+                $('.img-fail').removeClass('hide');
+                $('#message-delete').text(message);
+            }
+        },
+        failure: function (errMsg) {
+            console.log(errMsg);
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
 });
 
-$("#search").click(function (e) {
-    console.log("username = " + $('#searchByUsername input').val());
-    console.log("sortById = " + $('#sortBy input').attr("value"));
-    console.log("roleId = " + $('#role-name input').attr("value"));
-    console.log("orderById = " + $('#orderBy input').attr("value"));
+/*Reset Account*/
+$("#resetPassword").click(function (e) {
+    $('.resetPass-err').text("");
+    var newpassword = $('#new-password').val();
+    var confirmpassword = $('#confirm-password').val();
+
+    if (newpassword.trim() == "" && confirmpassword.trim() == "") {
+        $('.resetPass-err').text("Hãy điền đầy đủ tất cả các trường.");
+        return false;
+    } else if (newpassword.trim() == "") {
+        $('.resetPass-err').text("Hãy điền mật khẩu mới.");
+        return false;
+    } else if (confirmpassword.trim() == "") {
+        $('.resetPass-err').text("Hãy xác nhận lại mật khẩu.");
+        return false;
+    } else if (newpassword != confirmpassword) {
+        $('.resetPass-err').text("Mật khẩu xác nhận không đúng.");
+        return false;
+    } else {
+        var resetPassword = {
+            userNameList: listUser,
+            passWord: newpassword,
+        }
+        e.preventDefault();
+        $.ajax({
+            url: '/api/admin/resetpassword',
+            type: 'POST',
+            data: JSON.stringify(resetPassword),
+            success: function (data) {
+                var messageCode = data.messageCode;
+                var message = data.message;
+                if (messageCode == 0) {
+                    $('.img-success').removeClass('hide');
+                    $('.img-fail').addClass('hide');
+                    $('#message-reset').text(message);
+                } else {
+                    $('.img-success').addClass('hide');
+                    $('.img-fail').removeClass('hide');
+                    $('#message-reset').text(message);
+                }
+            },
+            failure: function (errMsg) {
+                console.log(errMsg);
+            },
+            dataType: "json",
+            contentType: "application/json"
+        });
+    }
 });
+
+/*Select checkbox*/
+function selectCheckbox() {
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $(checkbox).on('change', function (e) {
+        row = $(this).closest('tr');
+        if ($(this).is(':checked')) {
+            row.addClass('selected');
+            listUser.push($(this).val());
+            // console.log("My favourite sports are: " + userList.join(", "));
+        } else {
+            row.removeClass('selected');
+            var removeItem = $(this).val();
+            listUser = $.grep(listUser, function (value) {
+                return value != removeItem;
+            });
+        }
+        console.log(listUser);
+    });
+
+    $("#selectAll").click(function () {
+        if (this.checked) {
+            checkbox.each(function () {
+                this.checked = true;
+            });
+            $('tbody tr').addClass('selected');
+        } else {
+            checkbox.each(function () {
+                this.checked = false;
+            });
+            $('tbody tr').removeClass('selected');
+        }
+
+    });
+    checkbox.click(function () {
+        if (!this.checked) {
+            $("#selectAll").prop("checked", false);
+        }
+    });
+}
