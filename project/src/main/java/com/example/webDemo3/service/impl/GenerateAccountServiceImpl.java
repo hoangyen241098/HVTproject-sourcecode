a78dcedd8303174f54cc92319e4240f35fa4bce6
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2020-06-30 17:21:29 +0700

    kimpt142 delete function format not use 30/6

--- project/src/main/java/com/example/webDemo3/service/impl/GenerateAccountServiceImpl.java
+++ project/src/main/java/com/example/webDemo3/service/impl/GenerateAccountServiceImpl.java
@@ -91,21 +91,7 @@ public class GenerateAccountServiceImpl implements GenerateAccountService {
         return responseDto;
     }
 
-    private String formatString(String str){
-        str = str.toLowerCase();
-        str = str.replace("/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g", "a");
-        str = str.replace("/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g", "e");
-        str = str.replace("/ì|í|ị|ỉ|ĩ/g", "i");
-        str = str.replace("/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g", "o");
-        str = str.replace("/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g", "u");
-        str = str.replace("/ỳ|ý|ỵ|ỷ|ỹ/g", "y");
-        str = str.replace("/đ/g", "d");
-        str = str.replace("/\\s+/g", "");
-        str = str.trim();
-        return str;
-    }
-
-    public static String stripAccents(String s)
+    private String stripAccents(String s)
     {
         s = s.toLowerCase();
         s = Normalizer.normalize(s, Normalizer.Form.NFD);
