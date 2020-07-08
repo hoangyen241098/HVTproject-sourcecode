922d8072d9bfe34452075a812c39c305a9708ccd
Jung Ki Ha Ji <haji98.hjjk@gmail.com>
2020-07-08 11:21:58 +0700

    add function edit in table for rankByWeek

---
+++ project/src/main/resources/static/js/manageSchoolRank/rankByWeek.js
@@ -0,0 +1,17 @@
+$("#editGrades").on("click", function () {
+    var row = $('tbody tr td[contenteditable]');
+    var editOn = $('#editGrades').hasClass("editMode");
+
+    if (editOn == false) {
+        $(row).attr('contenteditable', 'true');
+        $(row).css('background-color', '#fdf1f1');
+        $('#editGrades').attr('value', 'Lưu thay đổi');
+        $('#editGrades').addClass('editMode');
+    } else if (editOn == true) {
+        $(row).attr('contenteditable', 'false');
+        $(row).css('background-color', 'transparent');
+        $('#editGrades').attr('value', 'Sửa điểm');
+        $('#editGrades').removeClass('editMode');
+    }
+});
+
