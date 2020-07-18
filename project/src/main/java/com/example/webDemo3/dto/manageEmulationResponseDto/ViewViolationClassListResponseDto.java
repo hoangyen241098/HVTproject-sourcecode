56c419664d66371e052be9ee3122fe554b3870ee
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-07-19 00:21:31 +0700

    lamnt98 commit fix function change request 19/07

--- project/src/main/java/com/example/webDemo3/dto/manageEmulationResponseDto/ViewViolationClassListResponseDto.java
+++ project/src/main/java/com/example/webDemo3/dto/manageEmulationResponseDto/ViewViolationClassListResponseDto.java
@@ -15,6 +15,5 @@ public class ViewViolationClassListResponseDto {
 
     List<ViolationClassResponseDto> viewViolationClassList;
     private Integer totalPage;
-    private Float substractGrade;
     private MessageDTO message;
 }
