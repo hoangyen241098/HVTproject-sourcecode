f5dc22397b6d254da2a29f96be432c20d8321741
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2020-07-21 08:49:17 +0700

    kimpt142 commit create package for manageSchoolRank 21/07

---
+++ project/src/main/java/com/example/webDemo3/service/impl/manageSchoolRankImpl/ViewSchoolRankWeekServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.webDemo3.service.impl.manageSchoolRankImpl;
+
+import org.springframework.stereotype.Service;
+
+/*
+kimpt142 - 21/07
+ */
+@Service
+public class ViewSchoolRankWeekServiceImpl {
+}
