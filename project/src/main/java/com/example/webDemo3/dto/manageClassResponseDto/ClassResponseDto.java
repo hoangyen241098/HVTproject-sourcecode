e9457b8e6c2329d9fe13d02cdb69933e94e5e232
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2020-07-22 21:27:55 +0700

    kimpt142 commit fix sort class 22/07

--- project/src/main/java/com/example/webDemo3/dto/manageClassResponseDto/ClassResponseDto.java
+++ project/src/main/java/com/example/webDemo3/dto/manageClassResponseDto/ClassResponseDto.java
@@ -3,12 +3,7 @@ package com.example.webDemo3.dto.manageClassResponseDto;
 import lombok.Data;
 
 @Data
-public class ClassResponseDto implements Comparable<ClassResponseDto> {
+public class ClassResponseDto {
     private Integer classID;
     private String className;
-
-    @Override
-    public int compareTo(ClassResponseDto o) {
-        return this.className.compareTo(o.getClassName());
-    }
 }
