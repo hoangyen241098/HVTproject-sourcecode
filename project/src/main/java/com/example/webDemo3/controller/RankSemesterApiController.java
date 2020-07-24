96444088e183fa27b8038ebe52366d9c506ef558
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2020-07-24 11:54:50 +0700

    kimpt142 commit add rank semester controller 24/07

--- project/src/main/java/com/example/webDemo3/controller/RankSemesterApiController.java
+++ project/src/main/java/com/example/webDemo3/controller/RankSemesterApiController.java
@@ -1,5 +1,15 @@
 package com.example.webDemo3.controller;
 
+import com.example.webDemo3.dto.MessageDTO;
+import com.example.webDemo3.dto.manageSchoolRankResponseDto.*;
+import com.example.webDemo3.dto.request.manageSchoolRankRequestDto.LoadByYearIdRequestDto;
+import com.example.webDemo3.dto.request.manageSchoolRankRequestDto.SearchRankSemesterRequestDto;
+import com.example.webDemo3.dto.request.manageSchoolRankRequestDto.UpdateSchoolRankWeekRequestDto;
+import com.example.webDemo3.service.manageSchoolRank.ViewSchoolRankWeekService;
+import com.example.webDemo3.service.manageSchoolRankSemesterService.ViewSchoolRankSemesterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +19,46 @@ kimpt142 - 23/07
 @RestController
 @RequestMapping("/api/ranksemester")
 public class RankSemesterApiController {
+
+    @Autowired
+    private ViewSchoolRankSemesterService viewSchoolRankSemesterService;
+
+    /**
+     * kimpt142
+     * 24/07
+     * catch request to get year list and semester list by year id
+     * @param model include current year id
+     * @return MessageDTO
+     */
+    @PostMapping("/loadranksemester")
+    public LoadRankSemesterResponseDto loadRankSemester(@RequestBody LoadByYearIdRequestDto model)
+    {
+        return viewSchoolRankSemesterService.loadRankSemesterPage(model);
+    }
+
+    /**
+     * kimpt142
+     * 24/07
+     * catch request to get semester list by year id
+     * @param model include year id
+     * @return ViewWeekListResponseDto
+     */
+    @PostMapping("/getsemesterlist")
+    public ViewSemesterListResponseDto getSemesterByYearId(@RequestBody LoadByYearIdRequestDto model)
+    {
+        return viewSchoolRankSemesterService.getSemesterListByYearId(model);
+    }
+
+    /**
+     * kimpt142
+     * 24/07
+     * catch request to get rank semester list by semester id
+     * @param model include semester id
+     * @return RankSemesterListResponseDto
+     */
+    @PostMapping("/searchranksemester")
+    public RankSemesterListResponseDto getSemesterByYearId(@RequestBody SearchRankSemesterRequestDto model)
+    {
+        return viewSchoolRankSemesterService.searchRankSemesterById(model);
+    }
 }
