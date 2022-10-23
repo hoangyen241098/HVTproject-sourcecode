ecc7bac492529e4065bfc9a2e650df9aeb0b3aa5
LoanNTK <haji98.hjjk@gmail.com>
2022-10-23 17:00:25 +0700

    Update library ckeditor

--- project/src/main/resources/static/js/public/ckeditor/src/ckeditor.js
+++ project/src/main/resources/static/js/public/ckeditor/src/ckeditor.js
@@ -13,7 +13,6 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox.js';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
@@ -24,6 +23,8 @@ import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtml
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import HtmlComment from '@ckeditor/ckeditor5-html-support/src/htmlcomment.js';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
@@ -32,17 +33,18 @@ import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import PictureEditing from '@ckeditor/ckeditor5-image/src/pictureediting.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
 import Style from '@ckeditor/ckeditor5-style/src/style.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
@@ -50,8 +52,6 @@ import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.j
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Title from '@ckeditor/ckeditor5-heading/src/title.js';
-import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 
@@ -69,7 +69,6 @@ Editor.builtinPlugins = [
 	CKBox,
 	CloudServices,
 	Code,
-	CodeBlock,
 	Essentials,
 	FindAndReplace,
 	FontBackgroundColor,
@@ -80,6 +79,8 @@ Editor.builtinPlugins = [
 	Heading,
 	Highlight,
 	HorizontalLine,
+	HtmlComment,
+	HtmlEmbed,
 	Image,
 	ImageCaption,
 	ImageInsert,
@@ -88,17 +89,18 @@ Editor.builtinPlugins = [
 	ImageToolbar,
 	ImageUpload,
 	Indent,
-	IndentBlock,
 	Italic,
 	Link,
 	LinkImage,
 	List,
+	ListProperties,
 	MediaEmbed,
 	PageBreak,
 	Paragraph,
 	PasteFromOffice,
 	PictureEditing,
 	RemoveFormat,
+	SpecialCharacters,
 	Style,
 	Table,
 	TableCaption,
@@ -106,8 +108,6 @@ Editor.builtinPlugins = [
 	TableProperties,
 	TableToolbar,
 	TextTransformation,
-	Title,
-	TodoList,
 	Underline,
 	WordCount
 ];
@@ -117,14 +117,6 @@ Editor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
-			'style',
-			'fontBackgroundColor',
-			'fontColor',
-			'fontFamily',
-			'fontSize',
-			'underline',
-			'highlight',
-			'removeFormat',
 			'|',
 			'bold',
 			'italic',
@@ -135,15 +127,24 @@ Editor.defaultConfig = {
 			'outdent',
 			'indent',
 			'alignment',
-			'|',
-			'undo',
-			'redo',
+			'fontBackgroundColor',
+			'fontColor',
+			'underline',
+			'fontFamily',
+			'fontSize',
+			'style',
+			'highlight',
+			'htmlEmbed',
+			'removeFormat',
+			'specialCharacters',
 			'findAndReplace',
-			'imageInsert',
+			'|',
+			'imageUpload',
 			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
-			'todoList'
+			'undo',
+			'redo'
 		]
 	},
 	language: 'vi',
